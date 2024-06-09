8f34cc2431d4658ccf9a3c684722c2b5fb77c1c0
Camille Sylla <camillesylla.web@gmail.com>
2024-06-10 01:41:11 +0200

    refactor: linting

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,14 +1,14 @@
-import { config } from 'dotenv';
-import { DataSourceOptions } from 'typeorm';
-import { SeederOptions } from 'typeorm-extension';
+import { config } from "dotenv";
+import { DataSourceOptions } from "typeorm";
+import { SeederOptions } from "typeorm-extension";
 
 config();
 
 export const dataSourceOptions: DataSourceOptions & SeederOptions = {
-  type: 'postgres',
-  url: process.env.DATABASE_URL || 'postgres://your_default_url', // Update the default URL as needed
-  entities: ['dist/**/*.entity.{ts,js}'],
+  type: "postgres",
+  url: process.env.DATABASE_URL || "postgres://your_default_url", // Update the default URL as needed
+  entities: ["dist/**/*.entity.{ts,js}"],
   synchronize: true,
-  seeds: ['dist/database/seeder/*.js'],
-  factories: ['dist/database/factories/*.js'],
+  seeds: ["dist/database/seeder/*.js"],
+  factories: ["dist/database/factories/*.js"],
 };
